649df0a58f9723e949a982f9b58cec8961df5b7d
RythenGlyth <rythenglyth@gmail.com>
2020-05-18 18:03:30 +0200

    gradle dynamic modid

--- src/main/java/de/bigmachines/Reference.java
+++ src/main/java/de/bigmachines/Reference.java
@@ -11,12 +11,13 @@ public class Reference {
 	public static final String MOD_ID = "bigmachines";
 	public static final String MOD_NAME = "Big Machines";
 	public static final String VERSION = "0.0.1";
+	public static final String PACKAGE = "de.bigmachines";
 
-	public static final String CLIENT_PROXY_CLASS = "de.bigmachines.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "de.bigmachines.proxy.ServerProxy";	
+	public static final String CLIENT_PROXY_CLASS = PACKAGE + ".proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = PACKAGE + ".proxy.ServerProxy";	
 	
-	public static final String GUI_FACTORY = "de.bigmachines.gui.ModGuiFactory";
+	public static final String GUI_FACTORY = PACKAGE + ".gui.ModGuiFactory";
 	
-	public static final String PATH_TEXTURES = Reference.MOD_ID +  ":textures/";
+	public static final String PATH_TEXTURES = MOD_ID +  ":textures/";
 	
 }
