8b6e7b0fd41957ee2dd0636a45c929da9e909a88
Lenni <lennivh24@gmail.com>
2020-05-24 11:05:31 +0200

    two helper methods

--- src/main/java/de/bigmachines/utils/FileHelper.java
+++ src/main/java/de/bigmachines/utils/FileHelper.java
@@ -3,11 +3,23 @@ package de.bigmachines.utils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.apache.logging.log4j.core.util.Loader;
+
+import de.bigmachines.config.ManualLoader;
 
 public class FileHelper {
 	
@@ -33,6 +45,39 @@ public class FileHelper {
 		}
 	}
 	
+	public static List<File> getResourcesFolder(String folder) {
+		List<File> files = new ArrayList<File>();
+		Stream<Path> walker;
+		try {
+			URI uri = ManualLoader.class.getResource(folder).toURI();
+	    	Path myPath;
+	    	if (uri.getScheme().equals("jar")) {
+	    		FileSystem fs = FileSystems.newFileSystem(uri, Collections.<String, Object> emptyMap());
+	    		myPath = fs.getPath(folder);
+	    	} else
+	    		myPath = Paths.get(uri);
+	    	
+	    	walker = Files.walk(myPath, 1);
+	    	for (Iterator<Path> it = walker.iterator(); it.hasNext(); ) {
+	    		File f = it.next().toFile();
+	    		if (f.isFile())
+	    			files.add(f);
+	    	}
+			walker.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return files;
+	}
+	
+	public static String getExtension(File f) {
+		String[] name = f.getName().split("\\.");
+		if (name.length > 0)
+			return name[name.length - 1];
+		else
+			return "";
+	}
+	
 	public static void copyFileUsingStreamAndLoader(String source, File dest) {
 		try {
 			InputStream inputStream = Loader.getResource(source, null).openStream();
@@ -42,7 +87,7 @@ public class FileHelper {
 			while ((length = inputStream.read(buffer)) > 0) {
 				outputStream.write(buffer, 0, length);
 			}
-			
+			inputStream.close();
 			outputStream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
