f6a2c20a94064cd1fbda4dc51c3a99b86fd69ae3
Lenni vH <lennivh24@gmail.com>
2020-05-31 12:53:08 +0000

    be gone

--- src/main/java/de/bigmachines/config/ManualLoader.java
+++ src/main/java/de/bigmachines/config/ManualLoader.java
@@ -1,28 +1,17 @@
 package de.bigmachines.config;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import de.bigmachines.gui.client.manual.ManualTab;
+import de.bigmachines.utils.FileHelper;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
-import org.jline.utils.InputStreamReader;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-
-import de.bigmachines.Reference;
-import de.bigmachines.gui.client.manual.ManualTab;
-import de.bigmachines.utils.FileHelper;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.IResource;
-import net.minecraft.util.ResourceLocation;
-
 public class ManualLoader {
-    private static final List<ManualTab> tabs = new ArrayList<ManualTab>();
+    private static final List<ManualTab> tabs = new ArrayList<>();
     public static Gson gson;
 
     public static List<ManualTab> getTabs() {
@@ -30,7 +19,6 @@ public class ManualLoader {
 	}
     
     public static void init() {
-		System.out.println("init");
     	gson = new GsonBuilder()
     			.registerTypeAdapter(ManualTab.class, new ManualTab.ManualDeserializer())
     	.create();
