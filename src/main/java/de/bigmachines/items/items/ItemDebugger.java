477973adf2021008e75cef41aae3697df87c4732
RythenGlyth <rythenglyth@gmail.com>
2020-07-17 12:41:12 +0200

    debugger creative tab

--- src/main/java/de/bigmachines/items/items/ItemDebugger.java
+++ src/main/java/de/bigmachines/items/items/ItemDebugger.java
@@ -1,11 +1,13 @@
 package de.bigmachines.items.items;
 
+import de.bigmachines.init.ModCreativeTabs;
 import de.bigmachines.items.ItemBase;
 
 public class ItemDebugger extends ItemBase {
 
 	public ItemDebugger() {
 		super("debugger");
+		setCreativeTab(ModCreativeTabs.modTab);
 	}
 	
 	
