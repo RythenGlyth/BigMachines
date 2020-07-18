1b4be066130fc6133f699ca9f2fe5921b8babf98
RythenGlyth <rythenglyth@gmail.com>
2020-07-18 12:15:23 +0200

    changed concept of ihudproviders

--- src/main/java/de/bigmachines/items/IHUDInfoProvider.java
+++ src/main/java/de/bigmachines/items/IHUDInfoProvider.java
@@ -1,10 +1,9 @@
 package de.bigmachines.items;
 
+import de.bigmachines.config.Config.HUDPostitions;
 import net.minecraft.item.ItemStack;
-
-import java.util.List;
-
-import de.bigmachines.handler.hud.elements.HUDElement;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Interface for Items which provide a hud
@@ -14,6 +13,14 @@ import de.bigmachines.handler.hud.elements.HUDElement;
 
 public interface IHUDInfoProvider {
 
-	void addHUDInfo(List<HUDElement> info, ItemStack item);
+	@SideOnly(Side.CLIENT)
+	/**
+	 * @param item
+	 * @param hudPosition
+	 * @param offset the offset (height of the elements drawn before)
+	 * @param partialTicks
+	 * @return the height of the element
+	 */
+	int drawHUDInfo(ItemStack item, HUDPostitions hudPosition, int offset, float partialTicks);
 	
 }
