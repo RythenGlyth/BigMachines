82c267f71c48301e0000e39db27976e75cf1c7fd
RythenGlyth <rythenglyth@gmail.com>
2020-12-06 13:40:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# BigMachines
+Adds many big multiblock structures which consume/produce RF.
+
+
+Finalize distributable jar with `gradlew build`, never run `gradlew jar`.
---
+++ src/main/java/de/bigmachines/BigMachines.java
@@ -0,0 +1,32 @@
+package de.bigmachines;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+
+@Mod(Reference.MOD_ID)
+public class BigMachines {
+	
+	public BigMachines() {
+		
+	}
+	
+	private void setup(final FMLCommonSetupEvent event) {
+		
+	}
+	
+	private void doClientStuff(final FMLClientSetupEvent event) {
+		
+	}
+	
+	private void enqueueIMC(final InterModEnqueueEvent event) {
+		
+	}
+	
+	private void processIMC(final InterModProcessEvent event) {
+		
+	}
+	
+}
