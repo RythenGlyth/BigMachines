5e88659983f7e21c4d300c688e1a43c52d183f05
Lenni vH <lennivh24@gmail.com>
2021-04-29 12:04:04 +0200

    update to fg4, need to move to official mappings

--- src/main/java/de/bigmachines/Reference.java
+++ src/main/java/de/bigmachines/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "bigmachines";
 	public static final String MOD_NAME = "Big Machines";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 	public static final String PACKAGE = "de.bigmachines";
 
 	//public static final String CLIENT_PROXY_CLASS = PACKAGE + ".proxy.ClientProxy";
